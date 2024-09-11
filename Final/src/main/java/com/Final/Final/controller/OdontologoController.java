40a30c71ee00e4e95dc30fe2336fd8a40069969c
Ailin <ailindoria80@gmail.com>
2024-09-10 20:06:57 -0500

    merge

--- Final/src/main/java/com/Final/Final/controller/OdontologoController.java
+++ Final/src/main/java/com/Final/Final/controller/OdontologoController.java
@@ -1,4 +1,42 @@
 package com.Final.Final.controller;
 
+import com.Final.Final.entity.Odontologo;
+import com.Final.Final.service.IOdontologoServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/odontologos")
 public class OdontologoController {
+    @Autowired
+    private IOdontologoServicio odontologoServicio;
+
+    @GetMapping("buscar/{id}")
+    public ResponseEntity<Odontologo> buscarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(odontologoServicio.buscarPorId(id));
+    }
+    @PostMapping
+    public ResponseEntity<Odontologo> guardar(@RequestBody Odontologo odontologo) {
+        return ResponseEntity.ok(odontologoServicio.guardar(odontologo));
+        //return "redirect:/odontologos";
+    }
+    //listar todos
+    @GetMapping
+    public ResponseEntity<List<Odontologo>> listarTodos() {
+        return ResponseEntity.ok(odontologoServicio.listarTodos());
+    }
+    @PutMapping("actualizar/{id}")
+    public String actualizar(@PathVariable Long id, @RequestBody Odontologo odontologo){
+        odontologo.setId(id);
+        odontologoServicio.actualizar(odontologo);
+        return "redirect:/odontologos";
+    }
+    @GetMapping("eliminar/{id}")
+    public String eliminar(@PathVariable Long id){
+        odontologoServicio.eliminar(id);
+        return "redirect:/odontologos";
+    }
 }
